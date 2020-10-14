552d5c565ca48abbd32e5fd0ce15d3321ba6fd63
Takahashi Yuya <takahashcom@gmail.com>
2020-10-14 18:18:28 +0900

    Updates

---
+++ precache-manifest.ef9731d5347a7a2d9491e71564c792b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2393cfb2ba0a41287ccfb3f8d395fbbf",
+    "url": "/todoapp-react/index.html"
+  },
+  {
+    "revision": "e0d64a31c9229e176a5b",
+    "url": "/todoapp-react/static/css/main.38735930.chunk.css"
+  },
+  {
+    "revision": "f5968af37d09cce78075",
+    "url": "/todoapp-react/static/js/2.700870f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp-react/static/js/2.700870f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d64a31c9229e176a5b",
+    "url": "/todoapp-react/static/js/main.392fac4e.chunk.js"
+  },
+  {
+    "revision": "d22d96de7b83955660e2",
+    "url": "/todoapp-react/static/js/runtime-main.41e09d89.js"
+  }
+]);
