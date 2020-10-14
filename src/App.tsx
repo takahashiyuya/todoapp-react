ddf3e502b5a5f29f563d1f391b7562600f77bde6
Takahashi Yuya <takahashcom@gmail.com>
2020-10-14 13:01:44 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Item = string;
+
+interface AppState {
+    list: Item[]
+}
+
+class App extends React.Component<any, AppState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            list: [
+                '顔を洗う',
+                'ベランダに出る',
+                '陽を浴びる',
+                'ストレッチor筋トレ',
+                'コーヒーを淹れる',
+                'ニュースを見る'
+            ],
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Form/>
+                <Todo list={this.state.list}/>
+            </div>
+        );
+    }
+}
+
+interface ListProps {
+    list: Item[]
+}
+
+function Todo(props: ListProps) {
+    return (
+        <div className="Todo">
+            <ul>
+                {props.list.map(item => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+class Form extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    handleKeyDown(e: any) {
+        const enterKeyCode = 13;
+        if (e.keyCode === enterKeyCode) {
+            console.log(this.props)
+        }
+    }
+
+    render() {
+        return (
+            <div className="Form">
+                <label>
+                    <input
+                        onKeyDown={(e) => this.handleKeyDown(e)}
+                        className="inputTodo"
+                    />
+                </label>
+            </div>
+        );
+    }
 }
 
 export default App;
